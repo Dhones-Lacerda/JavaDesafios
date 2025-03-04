b3f92c1ce2782c80ea7e8bd1dbee6211ee0ec087
Dhones-Lacerda <dhones.l.rocha@gmail.com>
2025-03-03 21:06:33 -0300

    Adicionando arquivos à pasta Desafio01

---
+++ Desafio01/src/Problem.java
@@ -0,0 +1,19 @@
+// Abaixo segue um exemplo de código que você pode ou não utilizar
+import java.util.Scanner;
+
+public class Problem {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A, B, PROD;
+		
+		System.out.println("Digite o primeiro valor: ");
+		A = sc.nextInt();
+		System.out.println("Digite o segundo valor: ");
+		B = sc.nextInt();
+		 //TODO: Complete os espaços em branco com uma possível solução para o desafio 
+		PROD =  A + B;
+
+		System.out.println("PROD = " + PROD);
+	}
+}
